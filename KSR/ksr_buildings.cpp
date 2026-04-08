6040a4804e616748bcf31e19dedc4be4005e8cc7
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-04-08 13:49:46 +0200

    It's actually YMIN and the gnd injection is not imporant at all.

--- KSR/ksr_buildings.cpp
+++ KSR/ksr_buildings.cpp
@@ -70,42 +70,20 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
-  // --- INJECTING GROUND POINTS ---
-  // Find the bounding box and the lowest Z
-  CGAL::Bbox_3 bbox = CGAL::bbox_3(point_set.points().begin(), point_set.points().end());
-  FT ground_z = bbox.zmin();
-  // // Oriented bbox
-  // std::array<Point_3, 8> obb_points;
-  // CGAL::oriented_bounding_box(point_set, obb_points, CGAL::parameters::use_convex_hull(true));
-
-  // Generate a grid of points to represent the ground plane
-  // Use a spacing relative to the bounding box size (e.g., 1% of the width)
-  FT length = std::max(bbox.xmax()-bbox.xmin(), bbox.ymax()-bbox.ymin());
-  FT spacing = length / 100.0;
-
-  for (FT x = bbox.xmin(); x <= bbox.xmax(); x += spacing) {
-    for (FT y = bbox.ymin(); y <= bbox.ymax(); y += spacing) {
-      point_set.insert(Point_3(x, y, ground_z), Vector_3(0, 0, 1));
-    }
-  }
-  fs::path gnd_path = outdir / "pc_w_gnd.ply";
-  CGAL::IO::write_point_set(gnd_path, point_set);
-  // -----------------------------------------
-
   std::map<typename KSR::KSP::Face_support, bool> external_nodes;
   // The bottom is solid (Inside)
-  external_nodes[KSR::KSP::Face_support::ZMIN] = true;
+  external_nodes[KSR::KSP::Face_support::YMIN] = true;
   // The top and sides are empty (Outside)
   external_nodes[KSR::KSP::Face_support::ZMAX] = false;
+  external_nodes[KSR::KSP::Face_support::ZMIN] = false;
   external_nodes[KSR::KSP::Face_support::XMIN] = false;
   external_nodes[KSR::KSP::Face_support::XMAX] = false;
-  external_nodes[KSR::KSP::Face_support::YMIN] = false;
   external_nodes[KSR::KSP::Face_support::YMAX] = false;
  
   auto param = CGAL::parameters::k_neighbors(8)
     .maximum_distance(0.1)          // the maximum distance from a point to a plane
     .maximum_angle(10)              // the maximum angle in degrees between the normal associated with a point and the normal of a plane
-    .minimum_region_size(20)        // the minimum number of points a region must have
+    .minimum_region_size(100)        // the minimum number of points a region must have
     .reorient_bbox(true)            // Setting reorient_bbox to true aligns the x-axis of the bounding box with the direction of the largest variation in horizontal direction of the input data while maintaining the z-axis.
     .regularize_parallelism(true)   // whether parallelism should be regularized or not
     .regularize_coplanarity(true)   // whether coplanarity should be regularized or not
@@ -115,7 +93,7 @@ int main(int argc, char** argv) {
 
   // Algorithm.
   KSR ksr(point_set, param);
-  ksr.detection_and_partition(3, param);
+  ksr.detection_and_partition(2, param);
 
   std::vector<Point_3> vtx;
   std::vector<std::vector<std::size_t> > polylist;
